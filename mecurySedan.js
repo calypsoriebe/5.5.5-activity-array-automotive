7b742f15be7cecee06cea1cbf8452de7e5d469f6
Calypso Kay Riebe <calypso@Calypsos-MBP.attlocal.net>
2022-05-31 13:21:12 -0400

    I fixed my errors

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,51 +1,6 @@
 //this includes the vehicle class as a module
-// below is imported vehicle class
-class Vehicle {
-  constructor(make, model, year, color, mileage) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    this.passenger = 0;
-    this.speed = 0;
-    this.mileage = mileage;
-    this.started = false;
-    this.numberOfWheels = 0;
-  }
-}
-//below will be the extend
-class car extends Vehicle {
-  constructor(make, model, year, color, mileage) {
-    super(make, model, year, color, mileage);
-    this.maxPassengers = 5;
-    this.passenger = 0;
-    this.numberOfWheels = 4;
-    this.maximumSpeed = 160;
-    this.fuel = 10;
-    this.scheduleService = false;
-  }
-  //method for seeing if people can go in car
-  loadPassenger(num) {
-    if (num < this.maxPassengers) {
-      console.log("room is available");
-    }
-  }
-  //I wasn't really sure what to write with the methods so I console log the methods
-  start() {
-    if (this.fuel > 0) {
-      console.log("engine is running and car is driving");
-    }
-  }
-  scheduleService(mileage) {
-    if (mileage > 30000) {
-      this.scheduleService === true;
-      console.log("Time for service");
-    }
-  }
-}
-
 const VehicleModule = require("./vehicleBaseClass");
-
+/*
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle(
   "Mecury",
@@ -55,3 +10,55 @@ let v = new VehicleModule.Vehicle(
   "mileage"
 );
 console.log(v.make);
+*/
+class Car extends VehicleModule {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.maxPassenger = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maxSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+
+  loadPassenger(num) {
+    if (this.passenger < this.maxPassenger) {
+      if (this.passenger + num <= this.maxPassenger) {
+        this.passenger += num;
+        return this.passenger;
+      } else {
+        console.log(
+          "There is not enough room for" + " " + this.passenger + " " + "people"
+        );
+      }
+    } else {
+      console.log("There is no more room");
+    }
+  }
+
+  start() {
+    if (this.fuel > 0) {
+      console.log("the car has started");
+      return (this.started = true);
+    } else {
+      console.log("No fuel");
+      return (this.started = false);
+    }
+  }
+
+  scheduleService(mileage) {
+    if (mileage > 30000) {
+      this.scheduleService = true;
+      console.log("Schedule service");
+    } else {
+      this.scheduleService = false;
+      console.log("no service needed");
+    }
+  }
+}
+
+let newCar = new Car("Mercury", "Sedan", "2010", "black", 25000);
+newCar.start();
+newCar.loadPassenger(6);
+newCar.scheduleService();
